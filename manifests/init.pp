93e1de8aa950268956640b0d81c74ad14a049785
John Florian <jflorian@doubledog.org>
2012-01-11 14:29:49 -0500

    Change - several sudo module improvements
    - no longer require passwords for sudo usage (*)
    - abstract ddo details out of module
    - style conformity
    
    (*) Rationale:
    
        Any user gaining access through a login could use sudo since they
        have already got the user's password.  An existing login could be
        compromised, but that requires physical access to that session and
        those are quite safe at home.  The biggest caveat here is that home
        shells from work shells should not be left unattended.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,27 +1,42 @@
 # modules/sudo/manifests/init.pp
+#
+# Synopsis:
+#       Configures sudo on a host.
+#
+# Parameters:
+#       NONE
+#
+# Requires:
+#       NONE
+#
+# Example usage:
+#
+#       include sudo
 
 class sudo {
 
-    package { "sudo":
+    package { 'sudo':
 	ensure	=> installed
     }
 
-    file { "/etc/sudoers":
-        group	=> "root",
-        mode    => "0440",
-        owner   => "root",
-        require => Package["sudo"],
-        source  => "puppet:///modules/sudo/sudoers",
+    file { '/etc/sudoers':
+        group	=> 'root',
+        mode    => '0440',
+        owner   => 'root',
+        require => Package['sudo'],
+        source  => 'puppet:///modules/sudo/sudoers',
     }
 
-    file { "/etc/sudoers.d/doubledog":
-        group	=> "root",
-        mode    => "0440",
-        owner   => "root",
-        require => Package["sudo"],
+    # NB: sudo will ignore files endinging in '~' or containing '.'.
+    file { "/etc/sudoers.d/sudoers-${hostname}":
+        group	=> 'root',
+        mode    => '0440',
+        owner   => 'root',
+        require => Package['sudo'],
         source  => [
-            "puppet:///modules/sudo/sudoers.$hostname",
-            "puppet:///modules/sudo/sudoers.doubledog",
+            "puppet:///private-host/sudo/sudoers-${hostname}",
+            "puppet:///private-domain/sudo/sudoers-${domain}",
+            'puppet:///modules/sudo/sudoers-default',
         ],
     }
 
